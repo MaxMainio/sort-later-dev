c4b891a0941ec5e3efe7a6d83157f869fb32469f
MaxMainio <beidler.max@gmail.com>
2023-01-31 21:56:30 -0500

    attempt 5 success and main page update

--- sort-later-test6/sources/script.js
+++ sort-later-test6/sources/script.js
@@ -1,15 +1,14 @@
 console.log('https://www.youtube.com/watch?v=NuAKnbIr6TE');
 
 function scaleType(){
-    var vw = window.innerWidth;
-    var vh = window.innerHeight;
+    var vw = window.innerWidth
+    var vh = window.innerHeight
     var texts = document.querySelectorAll('p');
     
     var i;
     for(i = 0; i < texts.length; i++){
-        var heightToFit = texts[i].parentElement.offsetHeight
-        texts[i].style.fontSize = heightToFit + 'px';
-        texts[i].style.lineHeight = heightToFit + 'px';
+        texts[i].style.fontSize = vh / texts.length + 'px';
+        texts[i].style.lineHeight = vh / texts.length + 'px';
 
         var lineWidth = texts[i].offsetWidth;
         var fitLength = vw / (lineWidth / 100);
@@ -17,12 +16,6 @@ function scaleType(){
         texts[i].style.transform = 'scaleX(' + fitLength + '%)';
     }
 }
-
-
-
-
-
-console.log(document.querySelector('#large').parentElement.offsetHeight)
 
 
 window.onload = (event) => {
