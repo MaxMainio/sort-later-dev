858efebac383546f5405338346292ae2f55790e3
MaxMainio <beidler.max@gmail.com>
2023-03-02 20:33:19 -0500

    working on post

--- post-show/sources/script.js
+++ post-show/sources/script.js
@@ -1,2 +1,21 @@
 console.log('https://www.youtube.com/watch?v=NuAKnbIr6TE');
 
+const exhibitionSlide = document.getElementById('exhibition')
+const workSlide = document.getElementById('work')
+const creditsSlide = document.getElementById('credits')
+
+
+exhibitionSlide.addEventListener('click', e => {
+    workSlide.classList.add('removed');
+    creditsSlide.classList.add('removed');
+})
+
+workSlide.addEventListener('click', e => {
+    creditsSlide.classList.add('removed');
+    workSlide.classList.remove('removed');
+})
+
+creditsSlide.addEventListener('click', e => {
+    creditsSlide.classList.remove('removed');
+    workSlide.classList.remove('removed');
+})
