8fe83589c44b5bcf0ff36271fcdd2de1a5f5ddb7
MaxMainio <beidler.max@gmail.com>
2023-03-03 13:36:58 -0500

    maybe gradient can go back inside section with 100% height and calculated negative left margin.

--- post-show/sources/script.js
+++ post-show/sources/script.js
@@ -2,12 +2,20 @@ console.log('https://www.youtube.com/watch?v=NuAKnbIr6TE');
 
 const favicon = document.querySelector("link[rel~='icon']");
 
-const exhibitionSlide = document.getElementById('exhibition')
-const workSlide = document.getElementById('work')
-const studentsSlide = document.getElementById('students')
+const exhibitionSlide = document.getElementById('exhibition');
+const workSlide = document.getElementById('work');
+const studentsSlide = document.getElementById('students');
 
-// const webGradient = document.getElementById('web-gradient');
 const falseFile = document.getElementById('false-file');
+
+const webGradient = document.getElementById('web-gradient');
+const studentSection = document.getElementById('student-section');
+
+
+
+
+
+
 // const monthSvg = document.getElementById('month-container');
 
 // const printGradient = document.getElementById('print-gradient');
@@ -17,15 +25,24 @@ const falseFile = document.getElementById('false-file');
 
 
 
-// ON LOAD ---------------------------------------------------------------------------------------------------------------
+// ON LOAD --------------------------------------------------------------------------------------------
 window.onload = (event) => {
-    falseFile.style.setProperty("background-image", getRandomWebDots());
     favicon.href = 'assets/favicons/favicon-' + randomIntFromInterval(1, 10) + '.ico';
+    falseFile.style.setProperty("background-image", getRandomWebDots());
+
+    webGradient.style.height = setWebGradientHeight();
+    webGradient.style.background = generateGradient();
 }
 
 
 
-// GENERATE DOTS    ------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+// GENERATE DOTS    -----------------------------------------------------------------------------------
 function getRandomWebDots() {
     var dotNum = randomIntFromInterval(1, 3);
     var dotPath = 'url(assets/web-svg/dots-web-' + dotNum.toString() + '.svg';
@@ -34,7 +51,13 @@ function getRandomWebDots() {
 
 
 
-// CLICK ON STUDENTS / CREDITS PAGE --------------------------------------------------------------------------------------
+
+
+
+
+
+
+// CLICK ON STUDENTS / CREDITS PAGE -------------------------------------------------------------------
 studentsSlide.addEventListener('click', e => {
     studentsSlide.classList.add('animatable');
     workSlide.classList.add('animatable');
@@ -48,7 +71,7 @@ studentsSlide.addEventListener('click', e => {
     },500);
 })
 
-// CLICK ON WORK PAGE ----------------------------------------------------------------------------------------------------
+// CLICK ON WORK PAGE ---------------------------------------------------------------------------------
 workSlide.addEventListener('click', e => {
     studentsSlide.classList.add('animatable');
     workSlide.classList.add('animatable');
@@ -62,7 +85,7 @@ workSlide.addEventListener('click', e => {
     },500);
 })
 
-// CLICK ON EXHIBITION PAGE ----------------------------------------------------------------------------------------------
+// CLICK ON EXHIBITION PAGE ---------------------------------------------------------------------------
 exhibitionSlide.addEventListener('click', e => {
     workSlide.classList.add('animatable');
     studentsSlide.classList.add('animatable');
@@ -78,14 +101,82 @@ exhibitionSlide.addEventListener('click', e => {
 
 
 
-// GENERATES RANDOM INTEGER BETWEEN TWO VALUES    ------------------------------------------------------------------------
-function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+
+
+
+
+
+
+// GENERATE GRADIENT    -------------------------------------------------------------------------------
+function generateGradient(){
+    // GETS RANDOM GRADIENT BY NAME AS STRING
+    var gradTag = 'gradient' + randomIntFromInterval(0, 4);
+    // ARRAY OF THE INDIVIDUAL COLORS OF SELECTED GRADIENT
+    var gradColors = eval(gradTag);
+    var gradSteps = gradColors.length;
+
+    var gradPlacements = [];
+    var min = 0;
+    var max = 100;
+    var offset = 0;
+
+    // THIS GENERATES THE POSITIONS FOR EACH OCLOR
+    for (var i = 0; i < gradSteps; i ++) {
+        var wiggleRoom = 10 * (gradSteps - i);
+
+        min = 0 + offset;
+        max = 100;
+        max = max - wiggleRoom;
+
+        placement = randomIntFromInterval(min, max);
+        gradPlacements[i] = placement
+        offset = placement + 10;
+    }
+
+    // PREPARE THE CSS EXPRESSION OF NEW GRADIENT
+    var lause = 'linear-gradient(' + flipACoin();
+
+    // THIS DECIDES DIRECTION OF THE GRADIENT
+    function flipACoin(){
+        var coinFlip = Math.round(Math.random());
+        if (coinFlip == 1) {
+            return '0deg';
+        } else {
+            return '180deg';
+        }
+    }
+
+    // GENERATES THE PROPER CSS EXPRESSION FOR THE GRADIENT
+    for (var i=0; i < gradSteps; i++) {
+        var appendable = gradColors[i] + ' ' + gradPlacements[i] + '%'
+        lause = lause + ', ' + appendable
+    }
+
+    // RETURN THE FORMULA FOR THE GRADIENT
+    return(lause + ')');
+}
+
+function setWebGradientHeight() {
+    var heightToSet = studentSection.offsetHeight;
+    webGradient.style.height = heightToSet + 'px';
 }
 
 
 
-// GRADIENTS    ----------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+// GENERATES RANDOM INTEGER BETWEEN TWO VALUES    -----------------------------------------------------
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+
+// GRADIENTS    ---------------------------------------------------------------------------------------
 const gradient0 = [];
 gradient0[0] = '#94ff3f';
 gradient0[1] = '#ffff00';
